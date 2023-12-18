4d6d426cc1632d16d89db93ad77ae3708ed7a0ca
voviethungit <voviethung.tk@gmail.com>
2023-12-18 18:07:22 +0700

    fix

--- src/viewtest/Pay/SuccessPage.jsx
+++ src/viewtest/Pay/SuccessPage.jsx
@@ -5,27 +5,6 @@ import axios from "axios";
 const SuccessPage = () => {
   const location = useLocation();
   
-  useEffect(() => {
-    const queryParams = new URLSearchParams(location.search);
-    const vnpAmount = queryParams.get("vnp_Amount");
-    const vnpTxnRef = queryParams.get("vnp_TxnRef");
-    const userId = queryParams.get("userId");
-
-    if (vnpAmount && vnpTxnRef && userId) {
-      axios.post('http://localhost:5000/update_balance', {
-        userId: userId,
-        amount: vnpAmount / 100, 
-      })
-      .then(response => {
-        console.log(response.data); 
-      })
-      .catch(error => {
-        console.error(error);
-
-      });
-    }
-  }, [location.search]);
-
   return (
     <div>
       <h1>Success Page</h1>
